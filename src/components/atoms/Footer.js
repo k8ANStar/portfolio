571ad90b74b9d27d185a16ea7155d19e60f1207c
k8ANStar <kstarchuk3@my.bcit.ca>
2025-01-21 12:28:40 -0800

    adding under construction, setting pages up, making header and footer

---
+++ src/components/atoms/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+	return (
+		<div className={styles.footer}>
+			<h2>Want to Conenct?</h2>
+			<p>Below are my links to connect!</p>
+		</div>
+	);
+}
