51f989da3860f1a62645d5c8cdbc6b5c7c9233a0
k8ANStar <kstarchuk3@my.bcit.ca>
2025-02-09 21:54:11 -0800

    fixing errors

--- src/app/projects/projectpages/questify/page.js
+++ src/app/projects/projectpages/questify/page.js
@@ -1,16 +1,13 @@
 import Header from "@/components/atoms/Header";
 import Footer from "@/components/atoms/Footer";
-import styles from "../page.module.css";
 
-export default function pojectQuestify() {
-    return (
-        <>
-            <Header />
-            <main >
-        
-            </main>
+export default function projectQuestify() {
+	return (
+		<>
+			<Header />
+			<main></main>
 
-            <Footer />
-        </>
-    );
+			<Footer />
+		</>
+	);
 }
